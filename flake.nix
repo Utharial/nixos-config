173b766939f6d526a88d5860fdda5580b89a09a0
Helorien <helorien@outlook.com>
2024-12-26 12:42:10 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "NixOS Configurations";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+
+    hardware.url = "github:NixOS/nixos-hardware/master";
+
+    vital-modules.url = "github:nixvital/vital-modules";
+    vital-modules.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, hardware, ... }
+  @inputs: {
+    nixosConfigurations = {
+
+      # As an example here is a nixos configuration for a framework laptop.
+      VLW-Test-001 = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          # Get the community maintained framework baseline
+          #nixos-hardware.nixosModules.framework-12th-gen-intel
+          #vital-modules.nixosModules.foundation
+          #vital-modules.nixosModules.laptop
+          #vital-modules.nixosModules.iphone-connect
+          ./machines/VLW-Test-001
+        ];
+      };
+
+      # You can define more machines here.
+    };
+  };
+}
