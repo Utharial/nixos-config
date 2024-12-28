fa677bf01c211357c7f786863b41e2ce8be589c0
Helorien <helorien@outlook.com>
2024-12-28 13:00:31 +0100

    init

--- flake.nix
+++ flake.nix
@@ -1,56 +1,39 @@
 {
-  description = "NixOS Configurations";
-
   inputs = {
-    # Import the desired Nix channel. Defaults to unstable, which uses a fully tested rolling release model.
-    #   You can find a list of channels at https://wiki.nixos.org/wiki/Channel_branches
-    #   To follow a different channel, replace `nixos-unstable` with the channel name, e.g. `nixos-24.05`.
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
-    
-    # NixOS hardware settings
+    # Nix pkg channels
+    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11";
+
+    # Nix hardware config
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    # Create and format partitions
+    # Disko usage for formating and partition
     disko = {
       url = "github:nix-community/disko";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    # SecureBoot support
-    lanzaboote = {
-      url = "github:nix-community/lanzaboote";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
-
-  outputs = { self, nixpkgs, hardware, ... }
-  @inputs: 
-  let
-      inherit (self) outputs;
-      stateVersion = "24.11";
-      username = "ark";
-
-      libx = import ./modules/lib {
-        inherit
-          self
-          inputs
-          outputs
-          stateVersion
-          username
-          ;
-      };
-    in
-    {
-    # Create nixosConfiguration for different systems
+  outputs = { self, nixpkgs, nixos-hardware }
+  @ inputs: let
+    inherit (self) outputs;
+    systems = [
+      "aarch64-linux"
+      "i686-linux"
+      "x86_64-linux"
+      "aarch64-darwin"
+      "x86_64-darwin"
+    ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in {
+    packages =
+      forAllSystems (system: import ./pkgs nixpkgs.legacyPackages.${system});
     nixosConfigurations = {
-      # Desktop
-      # Headless
-      VLW-Test-001 = libx.mkHost {
-        hostname = "VLW-Test-001";
+      m3-kratos-vm = nixpkgs.lib.nixosSystem {
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./system/x86-x64-linux/VLW-Test-001
+          inputs.disko.nixosModules.disko
+        ];
       };
-
-      # You can define more machines here.
     };
   };
 }
