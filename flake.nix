5e1c727b566e47ba088eb8ebe67f49ef14c03af6
Helorien <helorien@outlook.com>
2024-12-29 09:52:14 +0100

    init

--- flake.nix
+++ flake.nix
@@ -1,57 +1,75 @@
 {
-  inputs = {
-    # Nix pkg channels
-    nixpkgs.url = "github:NixOs/nixpkgs/nixos-24.11-small";
+  inputs.disko.url = "github:nix-community/disko";
+  inputs.disko.inputs.nixpkgs.follows = "nixpkgs";
 
-    # Nix hardware config
-    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
-
-    # Disko usage for formating and partition
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
-  outputs = { self, nixpkgs, nixos-hardware, ... }
-  @ inputs: let
-    inherit (self) outputs;
-
-      stateVersion = "24.11";
-      username = "ark";
-
-      libx = import ./lib {
-        inherit
-          self
-          inputs
-          outputs
-          stateVersion
-          username
-          ;
-      };
-
-      dependencies = [
+  outputs = { self, disko, nixpkgs }: {
+    nixosConfigurations.i7 = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ({ pkgs, ... }:
+        let
+          dependencies = [
             pkgs.stdenv.drvPath
             self.nixosConfigurations.nixos.config.system.build.toplevel
             self.nixosConfigurations.nixos.config.system.build.diskoScript
           ] ++ builtins.map (i: i.outPath) (builtins.attrValues self.inputs);
 
           closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
-  in {
+        in
+        {
+          environment.etc."install-closure".source = "${closureInfo}/store-paths";
 
-  environment.etc."install-closure".source = "${closureInfo}/store-paths";
+          environment.systemPackages = [
+            (pkgs.writeShellScriptBin "install-nixos-unattended" ''
+              set -eux
+              # Replace "/dev/disk/by-id/some-disk-id" with your actual disk ID
+              exec ${pkgs.disko}/bin/disko-install --flake "${self}#nixos" --disk vdb /dev/sda "$@"
+            '')
+          ];
+        })
 
-  environment.systemPackages = [
-    (pkgs.writeShellScriptBin "install-nixos-unattended" ''
-      set -eux
-      # Replace "/dev/disk/by-id/some-disk-id" with your actual disk ID
-      exec ${pkgs.disko}/bin/disko-install --flake "${self}#${hostname}" --disk my-disk "/dev/sda"
-    '')
-  ];
-
-    nixosConfigurations = {
-      vlw-test-001 = libx.mkHost {
-        hostname = "vlw-test-001";
-      };
+      ];
     };
+
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        {
+          boot.loader.systemd-boot.enable = true;
+          imports = [ self.inputs.disko.nixosModules.disko ];
+          disko.devices = {
+            disk = {
+              vdb = {
+                device = "/dev/disk/by-id/some-disk-id";
+                type = "disk";
+                content = {
+                  type = "gpt";
+                  partitions = {
+                    ESP = {
+                      type = "EF00";
+                      size = "500M";
+                      content = {
+                        type = "filesystem";
+                        format = "vfat";
+                        mountpoint = "/boot";
+                      };
+                    };
+                    root = {
+                      size = "100%";
+                      content = {
+                        type = "filesystem";
+                        format = "ext4";
+                        mountpoint = "/";
+                      };
+                    };
+                  };
+                };
+              };
+            };
+          };
+        }
+      ];
+    };
+
   };
 }
