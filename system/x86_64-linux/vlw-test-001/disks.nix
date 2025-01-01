1c675957e9690239a3efbcc1ee99657c76f68af5
Helorien <helorien@outlook.com>
2025-01-01 13:14:42 +0100

    init

--- system/x86_64-linux/vlw-test-001/disks.nix
+++ system/x86_64-linux/vlw-test-001/disks.nix
@@ -1,32 +1,27 @@
 {
   disko.devices = {
     disk = {
-      main = {
+      my-disk = {
+        device = "/dev/sda";
         type = "disk";
-        device = "/dev/disk/by-diskseq/1";
         content = {
           type = "gpt";
           partitions = {
             ESP = {
-              priority = 1;
-              name = "ESP";
-              start = "1M";
-              end = "128M";
               type = "EF00";
+              size = "500M";
               content = {
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
               };
             };
             root = {
               size = "100%";
               content = {
-                type = "btrfs";
-                extraArgs = [ "-f" ]; # Override existing partition
+                type = "filesystem";
+                format = "ext4";
                 mountpoint = "/";
-                mountOptions = [ "compress=zstd" "noatime" ];
               };
             };
           };
