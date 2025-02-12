8f99f096bb7c4afe2f2327952cd1f785c956a95d
Helorien <helorien@outlook.com>
2025-02-12 15:21:57 +0100

    init

--- scripts/install-os.sh
+++ scripts/install-os.sh
@@ -22,14 +22,14 @@ fi
 #    exit 1
 #fi
 
-if [ ! -e "system/x86_64-linux/${TARGET_HOST}/disks.nix" ]; then
+if [ ! -e "../system/x86_64-linux/${TARGET_HOST}/disks.nix" ]; then
   echo "ERROR! $(basename "${0}") could not find the required system/x86_64-linux/${TARGET_HOST}/disks.nix"
   exit 1
 fi
 
 # Check if the machine we're provisioning expects a keyfile to unlock a disk.
 # If it does, generate a new key, and write to a known location.
-if grep -q "root.keyfile" "system/x86_64-linux/${TARGET_HOST}/disks.nix"; then
+if grep -q "root.keyfile" "../system/x86_64-linux/${TARGET_HOST}/disks.nix"; then
   #echo -n "$(head -c32 /dev/random | base64)" > /tmp/root.keyfile
   echo -n "$(head -c1 /dev/random | base64)" > /tmp/root.keyfile
 fi
@@ -47,7 +47,7 @@ if [[ $REPLY =~ ^[Yy]$ ]]; then
        --experimental-features "nix-command flakes" \
        -- \
        --mode zap_create_mount \
-        "system/x86_64-linux/${TARGET_HOST}/disks.nix"
+        "../system/x86_64-linux/${TARGET_HOST}/disks.nix"
 
     # Generate hardware-configuration.nix and copy to our working directory
     sudo nixos-generate-config --root /mnt
