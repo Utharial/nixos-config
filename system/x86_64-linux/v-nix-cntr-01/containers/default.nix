a8363d8947121c736b01763f50d7a3824aca6847
Helorien <helorien@outlook.com>
2025-03-08 10:44:37 +0100

    init

--- system/x86_64-linux/v-nix-cntr-01/containers/default.nix
+++ system/x86_64-linux/v-nix-cntr-01/containers/default.nix
@@ -1,33 +1,7 @@
-{
-  containers.unifi = {
-    autoStart = true;
-    ephemeral = true;
 
-    # Bind mount for persistent storage
-    bindMounts = {
-      "/persist/var/lib/unifi" = {
-        hostPath = "/var/lib/unifi-data"; # Persistent folder on the host
-        isReadOnly = false; # Allow writes to the directory
-      };
-    };
-
-    config = { config, pkgs, ... }: {
-      environment.systemPackages = with pkgs; [
-        unifi
-      ];
-
-      # Allow unfree packages
-      config.allowUnfree = true;
-
-      services.unifi = {
-        enable = true;
-        openFirewall = true;
-      };
-
-      networking.firewall = {
-        allowedTCPPorts = [ 8080 8443 8880 8843 6789 ];
-        allowedUDPPorts = [ 3478 10001 ];
-      };
-    };
-  };
+{pkgs, ... } : {
+  imports =
+    [
+      ./unifi.nix
+    ];
 }
